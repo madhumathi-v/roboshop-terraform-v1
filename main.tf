cb5ca5fe105a5116d37e9d4d175a88ae865d2020
Madhu-classet <madhumathi.v@classet.in>
2026-06-03 21:55:25 +0530

    Update VM size description for clarity

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_linux_virtual_machine" "frontend" {
   name                = "frontend"
   resource_group_name = "Denmark_East"
   location            = "Denmark East"
-  size                = "Standard D2s v3 (2 vcpus, 8 GiB memory)"
+  size                = "Standard D2s v3"
   network_interface_ids = [azurerm_network_interface.frontend.id ]
   
   source_image_id = "/subscriptions/3c3ac820-a526-4fd5-841f-cbb2d7ffa483/resourceGroups/Denmark_East/providers/Microsoft.Compute/galleries/image_vm/images/image"
